c7a32cb8afd3eb570d8107694c1c8eeb42ff5d0b
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-17 10:53:19 -0300

    Create 1071 - Soma de Impares Consecutivos I.c

---
+++ Iniciante/1071 - Soma de Impares Consecutivos I.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	
+	int X,Y,i,aux,soma_impar=0;
+	scanf("%d %d",&X,&Y);
+	
+	if(Y<X)
+	{
+		aux = X;
+		X = Y+1;
+		Y = aux;
+	}
+	for(i=X;i<Y;i++){
+		if(i%2!=0){
+			if(i<0){
+				soma_impar-=i*(-1);
+			}else{
+				soma_impar+=i;
+			}
+		}
+	}
+	printf("%d\n",soma_impar);
+	return 0;
+}
