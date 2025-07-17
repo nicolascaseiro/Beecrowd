4faa2d535ddf11a4aa4e40f97712a75fb170b7cd
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-17 10:36:05 -0300

    Create 1043 - Triângulo.c

---
+++ Iniciante/1043 - Triângulo.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    double A, B, C;
+
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    if(A < B + C && B < A + C && C < A + B){
+        printf("Perimetro = %.1lf\n", A + B + C);
+    }else{
+        printf("Area = %.1lf\n", (A + B)/2 * C);
+    }
+
+    return 0;
+}
