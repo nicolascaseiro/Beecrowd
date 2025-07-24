ad753ebbea5c166d0d8921b674c181597063941e
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-24 15:09:09 -0300

    Create  2863 - Umil Bolt.c

---
+++ Iniciante/2863 - Umil Bolt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main ()
+{
+
+	int casos;
+	float numero, menor;
+
+	while (scanf("%d", &casos) != EOF)
+	{
+
+		menor = 100.0f;
+		for (int i = 0; i < casos; ++i)
+		{
+			scanf("%f", &numero);
+			if (numero < menor)
+				menor = numero;
+
+		}
+
+		printf("%.2f\n", menor);
+
+	}
+    
+    return 0;
+}
