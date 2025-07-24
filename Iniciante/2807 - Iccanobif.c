a562cd5d727bb734919f4acd09a58cbc0652be9f
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-24 16:48:59 -0300

    Create  2807 - Iccanobif.c

---
+++ Iniciante/2807 - Iccanobif.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{
+
+	int i;
+	unsigned n;
+	long long fibonacci[100] = { 0 };
+
+	fibonacci[0] = 1;
+	fibonacci[1]= 1;
+
+	scanf("%u", &n);
+
+	for (i = 2; i < n; ++i)
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+	for (i = n - 1; i >= 1; --i)
+		printf("%lld ", fibonacci[i]);
+
+	printf("%lld\n", fibonacci[i]);
+    
+    return 0;
+}
